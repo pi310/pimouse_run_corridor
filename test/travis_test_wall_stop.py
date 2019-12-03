b355195474c86523028863d3b24e6a45af6a485e
pi310 <s1611130@st.tohtech.ac.jp>
2019-12-03 07:22:10 +0000

    test

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -19,13 +19,13 @@ class WallStopTest(unittest.TestCase):
 
     def test_io(self):
         left, right = self.set_and_get(400,100,100,0) #total: 600
-        self.assertTrue(left == 0 and right == 0,"cant't stop")
+        self.assertTrue(left == 0 and right == 0,"can't stop")
 
         left, right = self.set_and_get(400,0,0,99) #total: 499
-        self.assertTrue(left != 0 and right != 0,"cant't move again")
+        self.assertTrue(left != 0 and right != 0,"can't move again")
 
         left, right = self.set_and_get(150,0,200,150) #total: 500
-        self.assertTrue(left == 0 and right == 0,"cant't stop")
+        self.assertTrue(left == 0 and right == 0,"can't stop")
 
 if __name__ == '__main__':
     time.sleep(3)
